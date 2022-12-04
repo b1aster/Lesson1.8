11ca819d6fa43b3b7bbc294d857066d573136e94
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-12-04 20:30:55 +0500

    Решение задачи 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        int [] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        double [] weights = {21.4f, 23.f, 32.1f};
+        char [] message = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+
     }
 }
