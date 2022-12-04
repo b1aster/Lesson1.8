3294c785abda41a47b4475029c01843adecad4e9
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-12-04 20:41:49 +0500

    Решение задачи 2

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,38 @@ public class Main {
         numbers[1] = 2;
         numbers[2] = 3;
 
-        double [] weights = {21.4f, 23.f, 32.1f};
+        float [] weights = {21.4f, 23.f, 32.1f};
         char [] message = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
 
+        // Задача 2
+        for (int idx = 0; idx < numbers.length; idx++){
+
+            if (idx > 0 && idx < numbers.length)
+                System.out.print(", ");
+
+            System.out.print(numbers[idx]);
+        }
+
+        System.out.println();
+
+        for (int idx = 0; idx < weights.length; idx++){
+
+            if (idx > 0 && idx < weights.length)
+                System.out.print(", ");
+
+            System.out.printf("%.2f", weights[idx]);
+        }
+
+        System.out.println();
+
+        int idx = 0;
+        for (char ch : message){
+
+            if (idx > 0 && idx < message.length)
+                System.out.print(", ");
+
+            System.out.print(ch);
+            idx++;
+        }
     }
 }
